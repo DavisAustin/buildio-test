795ee0649296a21b965af8e020cd82395db70c93
Austin Davis <Austin.DavisDesigns@Gmail.Com>
2025-04-17 16:17:32 -0700

    Update badge.tsx

--- components/ui/badge/badge.tsx
+++ components/ui/badge/badge.tsx
@@ -5,18 +5,18 @@ import { cva, type VariantProps } from "class-variance-authority"
 import { cn } from "@/lib/utils"
 
 const badgeVariants = cva(
-  "inline-flex items-center justify-center rounded-md border px-2 py-0.5 text-xs font-medium w-fit whitespace-nowrap shrink-0 [&>svg]:size-3 gap-1 [&>svg]:pointer-events-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive transition-[color,box-shadow] overflow-hidden",
+  "inline-flex items-center justify-center rounded-default border px-2 py-0.5 text-body-sm w-fit whitespace-nowrap shrink-0 [&>svg]:size-3 gap-1 [&>svg]:pointer-events-none focus-visible-state invalid-state transition-[color,box-shadow] overflow-hidden",
   {
     variants: {
       variant: {
         default:
-          "border-transparent bg-primary text-primary-foreground [a&]:hover:bg-primary/90",
+          "border-border-accent bg-bg-accent text-fg-accent [a&]:hover:bg-bg-accent-hover",
         secondary:
-          "border-transparent bg-secondary text-secondary-foreground [a&]:hover:bg-secondary/90",
+          "border-border-default bg-bg-muted text-fg-default [a&]:hover:bg-bg-subtle-hover",
         destructive:
-          "border-transparent bg-destructive text-white [a&]:hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60",
+          "border-border-danger bg-bg-danger text-fg-danger [a&]:hover:bg-bg-danger-hover focus-visible-danger-state",
         outline:
-          "text-foreground [a&]:hover:bg-accent [a&]:hover:text-accent-foreground",
+          "text-fg-default [a&]:hover:bg-bg-default-hover",
       },
     },
     defaultVariants: {
