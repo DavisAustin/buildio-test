65ec38ef44dbc896b4e18017ab15322aa3f11643
Austin Davis <Austin.DavisDesigns@Gmail.Com>
2025-04-17 16:17:25 -0700

    Updated alert

--- components/ui/alert/alert.stories.tsx
+++ components/ui/alert/alert.stories.tsx
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from "@storybook/react";
-import { AlertCircle } from "lucide-react";
+import { AlertCircle, Info } from "lucide-react";
 
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert/alert";
 
@@ -21,6 +21,7 @@ const meta = {
   },
   render: (args) => (
     <Alert {...args}>
+      <Info className="size-4 text-fg-info" />
       <AlertTitle>Heads up!</AlertTitle>
       <AlertDescription>
         You can add components to your app using the cli.
@@ -43,7 +44,7 @@ export const Default: Story = {};
 export const Destructive: Story = {
   render: (args) => (
     <Alert {...args}>
-      <AlertCircle className="h-4 w-4" />
+      <AlertCircle className="size-4 text-fg-danger" />
       <AlertTitle>Error</AlertTitle>
       <AlertDescription>
         Your session has expired. Please log in again.
