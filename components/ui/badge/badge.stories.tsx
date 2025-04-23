8bbe408749fbae07e349e1319489c61caeef3afe
Austin Davis <Austin.DavisDesigns@Gmail.Com>
2025-04-23 10:51:00 -0700

    Badge updates
    Created the Code Connect file
    Updated the Storybook file
    Updated the Badge component styles

--- components/ui/badge/badge.stories.tsx
+++ components/ui/badge/badge.stories.tsx
@@ -1,6 +1,7 @@
 import type { Meta, StoryObj } from "@storybook/react";
 
 import { Badge } from "@/components/ui/badge/badge";
+import { Calendar } from "lucide-react";
 
 /**
  * Displays a badge or a component that looks like a badge.
@@ -27,9 +28,9 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 /**
- * The default form of the badge.
+ * The `primary` form of the badge.
  */
-export const Default: Story = {};
+export const Primary: Story = {};
 
 /**
  * Use the `secondary` badge to call for less urgent information, blending
@@ -42,21 +43,94 @@ export const Secondary: Story = {
 };
 
 /**
- * Use the `destructive` badge to  indicate errors, alerts, or the need for
- * immediate attention.
+ * Use the `emphasis` badge to  indicate emphasis and important information.
  */
-export const Destructive: Story = {
+export const Emphasis: Story = {
   args: {
-    variant: "destructive",
+    variant: "emphasis",
   },
 };
 
 /**
- * Use the `outline` badge for overlaying without obscuring interface details,
- * emphasizing clarity and subtlety..
+ * Use the `accent` badge to  indicate...
  */
-export const Outline: Story = {
+export const Accent: Story = {
   args: {
-    variant: "outline",
+    variant: "accent",
+  },
+};
+
+/**
+ * Use the `info` badge to  indicate information for the user.
+ */
+export const Info: Story = {
+  args: {
+    variant: "info",
+  },
+};
+
+/**
+ * Use the `new` badge to  indicate new features or updates.
+ */
+export const New: Story = {
+  args: {
+    variant: "new",
+  },
+};
+
+/**
+ * Use the `success` badge to  indicate success and confirmations.
+ */
+export const Success: Story = {
+  args: {
+    variant: "success",
+  },
+};
+
+/**
+ * Use the `attention` badge to  indicate attention and warnings.
+ */
+export const Attention: Story = {
+  args: {
+    variant: "attention",
+  },
+};
+
+/**
+ * Use the `danger` badge to  indicate danger and errors.
+ */
+export const Danger: Story = {
+  args: {
+    variant: "danger",
+  },
+};
+
+/**
+ * Use the `sm` badge for high density interfaces.
+ */
+export const Small: Story = {
+  args: {
+    variant: "primary",
+    size: "sm",
+  },
+};
+
+/**
+ * Use the `lg` badge for low density interfaces.
+ */
+export const Large: Story = {
+  args: {
+    variant: "primary",
+    size: "lg",
+  },
+};
+
+/**
+ * This is the badge with an icon.
+ */
+export const WithIcon: Story = {
+  render: (args) => <Badge {...args}><Calendar />Calendar</Badge>,
+  args: {
+    variant: "primary",
   },
 };
--- components/ui/badge/badge.tsx
+++ components/ui/badge/badge.tsx
@@ -1,46 +1,60 @@
-import * as React from "react"
-import { Slot } from "@radix-ui/react-slot"
-import { cva, type VariantProps } from "class-variance-authority"
+import * as React from "react";
+import { Slot } from "@radix-ui/react-slot";
+import { cva, type VariantProps } from "class-variance-authority";
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
 
 const badgeVariants = cva(
-  "inline-flex items-center justify-center rounded-default border px-2 py-0.5 text-body-sm w-fit whitespace-nowrap shrink-0 [&>svg]:size-3 gap-1 [&>svg]:pointer-events-none focus-visible-state invalid-state transition-[color,box-shadow] overflow-hidden",
+  "inline-flex items-center justify-center rounded-default w-fit whitespace-nowrap shrink-0 [&>svg]:pointer-events-none focus-visible-state invalid-state transition-[color,box-shadow] overflow-hidden",
   {
     variants: {
       variant: {
-        default:
-          "border-border-accent bg-bg-accent text-fg-accent [a&]:hover:bg-bg-accent-hover",
-        secondary:
-          "border-border-default bg-bg-muted text-fg-default [a&]:hover:bg-bg-subtle-hover",
-        destructive:
-          "border-border-danger bg-bg-danger text-fg-danger [a&]:hover:bg-bg-danger-hover focus-visible-danger-state",
-        outline:
-          "text-fg-default [a&]:hover:bg-bg-default-hover",
+        primary:
+          "border bg-bg-default text-fg-default [a&]:hover:bg-bg-default-hover",
+        secondary: "bg-bg-muted text-fg-default [a&]:hover:bg-bg-subtle-hover",
+        emphasis:
+          "bg-bg-emphasis text-fg-on-emphasis [a&]:hover:bg-bg-emphasis-hover",
+        accent:
+          "bg-bg-accent-emphasis text-fg-on-emphasis [a&]:hover:bg-bg-accent-emphasis-hover",
+        info: "bg-bg-info-emphasis text-fg-on-emphasis [a&]:hover:bg-bg-info-emphasis-hover",
+        new: "bg-bg-new-emphasis text-fg-on-emphasis [a&]:hover:bg-bg-new-emphasis-hover",
+        success:
+          "bg-bg-success-emphasis text-fg-on-emphasis [a&]:hover:bg-bg-success-emphasis-hover",
+        attention:
+          "bg-bg-attention-emphasis text-fg-on-emphasis [a&]:hover:bg-bg-attention-emphasis-hover",
+        danger:
+          "bg-bg-danger-emphasis text-fg-on-emphasis [a&]:hover:bg-bg-danger-emphasis-hover",
+      },
+      size: {
+        sm: "text-body-sm min-w-5 h-5 px-1 gap-0.5 [&>svg]:size-3",
+        default: "text-body-md min-w-6 h-6 px-2 gap-1 [&>svg]:size-3.5",
+        lg: "text-body-lg min-w-7 h-7 p-2 gap-1 [&>svg]:size-4",
       },
     },
     defaultVariants: {
-      variant: "default",
+      variant: "primary",
+      size: "default",
     },
-  }
-)
+  },
+);
 
 function Badge({
   className,
   variant,
+  size,
   asChild = false,
   ...props
 }: React.ComponentProps<"span"> &
   VariantProps<typeof badgeVariants> & { asChild?: boolean }) {
-  const Comp = asChild ? Slot : "span"
+  const Comp = asChild ? Slot : "span";
 
   return (
     <Comp
       data-slot="badge"
-      className={cn(badgeVariants({ variant }), className)}
+      className={cn(badgeVariants({ variant, size }), className)}
       {...props}
     />
-  )
+  );
 }
 
-export { Badge, badgeVariants }
+export { Badge, badgeVariants };
