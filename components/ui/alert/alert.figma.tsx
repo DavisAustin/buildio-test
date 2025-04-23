ba99f4df506d2900cc20470d313be1cb7f8e0e8e
Austin Davis <Austin.DavisDesigns@Gmail.Com>
2025-04-23 10:49:24 -0700

    Create alert.figma.tsx
    Created the Alert Code Connect file.

---
+++ components/ui/alert/alert.figma.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Alert, AlertTitle, AlertDescription } from "./alert";
+import { CircleAlert, Info } from "lucide-react";
+import { figma } from "@figma/code-connect";
+
+figma.connect(
+  Alert,
+  "https://www.figma.com/design/7gFdOSsbUxcHxyp84Ql45Z/%F0%9F%8C%90-OPL-Components-Beta?node-id=182-585&t=fj0kKDbslWnVPTgK-11",
+  {
+    props: {
+      variant: figma.enum("variant", {
+        default: <Info className="text-fg-info size-4" />,
+        danger: <CircleAlert className="text-fg-danger size-4" />,
+      }),
+      alertTitle: figma.string("alertTitle"),
+      alertDescription: figma.string("alertDescription"),
+    },
+    example: ({ variant, alertTitle, alertDescription }) => (
+      <Alert>
+        {variant}
+        <AlertTitle>{alertTitle}</AlertTitle>
+        <AlertDescription>
+          {alertDescription}
+        </AlertDescription>
+      </Alert>
+    ),
+  },
+);
