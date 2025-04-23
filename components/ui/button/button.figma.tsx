ef4133bc4ea1ff222d78993743c2efef01db85da
Austin Davis <Austin.DavisDesigns@Gmail.Com>
2025-04-23 10:51:13 -0700

    Update button.figma.tsx
    Updated props

--- components/ui/button/button.figma.tsx
+++ components/ui/button/button.figma.tsx
@@ -7,7 +7,6 @@ figma.connect(
   "https://www.figma.com/design/7gFdOSsbUxcHxyp84Ql45Z/%F0%9F%8C%90-OPL-Components-Beta?node-id=13%3A7982",
   {
     props: {
-      label: figma.textContent("Button"),
       disabled: figma.enum("state", {
         disabled: true,
       }),
@@ -28,13 +27,13 @@ figma.connect(
       leadingVisual: figma.instance("leadingVisual"),
       trailingVisual: figma.instance("trailingVisual")
     },
-    example: (props) => (
+    example: ({disabled, variant, size, leadingVisual, trailingVisual}) => (
       <Button
-        disabled={props.disabled}
-        variant={props.variant}
-        size={props.size}
-        leadingVisual={props.leadingVisual}
-        trailingVisual={props.trailingVisual}
+        disabled={disabled}
+        variant={variant}
+        size={size}
+        leadingVisual={leadingVisual}
+        trailingVisual={trailingVisual}
       />
     ),
   },
