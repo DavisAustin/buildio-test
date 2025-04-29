f1dca9d48862bfdb1a5d782934949380b72314c1
Austin Davis <Austin.DavisDesigns@Gmail.Com>
2025-04-29 09:43:38 -0700

    Update calendar.tsx

--- components/ui/calendar/calendar.tsx
+++ components/ui/calendar/calendar.tsx
@@ -5,7 +5,7 @@ import { ChevronLeft, ChevronRight } from "lucide-react";
 import { DayPicker } from "react-day-picker"
  
 import { cn } from "@/lib/utils"
-import { buttonVariants } from "@/components/ui/button/button"
+import { buttonVariants } from "@/components/ui/icon-button/icon-button"
  
 export type CalendarProps = React.ComponentProps<typeof DayPicker>
  
@@ -23,18 +23,17 @@ function Calendar({
         months: "flex flex-col sm:flex-row space-y-4 sm:space-x-4 sm:space-y-0",
         month: "space-y-4",
         caption: "flex justify-center pt-1 relative items-center",
-        caption_label: "text-sm font-medium",
+        caption_label: "text-body-sm",
         nav: "space-x-1 flex items-center",
         nav_button: cn(
-          buttonVariants({ variant: "secondary" }),
-          "h-7 w-7 bg-transparent p-0 opacity-50 hover:opacity-100"
+          buttonVariants({ variant: "tertiary" })
         ),
         nav_button_previous: "absolute left-1",
         nav_button_next: "absolute right-1",
         table: "w-full border-collapse space-y-1",
         head_row: "flex",
         head_cell:
-          "text-muted-foreground rounded-md w-8 font-normal text-[0.8rem]",
+          "text-fg-muted rounded-md w-8 text-body-sm font-regular",
         row: "flex w-full mt-2",
         cell: cn(
           "relative p-0 text-center text-sm focus-within:relative focus-within:z-20 [&:has([aria-selected])]:bg-accent [&:has([aria-selected].day-outside)]:bg-accent/50 [&:has([aria-selected].day-range-end)]:rounded-r-md",
